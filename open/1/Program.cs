e264831346d545035fbc29f9beb28dffb2cb8d1a
tongjr1 <512519134@qq.com>
2017-11-06 13:36:41 +0800

    change

---
+++ open/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 2, 3,2 };
+            int[] num = new int[nums.Length-1];
+            Array.Copy(nums, 1, num, 0, 3);
+            var a = nums.Contains(1);
+            var b = Array.IndexOf(nums,2);
+            Console.WriteLine(nums.Max());
+            Console.ReadKey();
+        }
+    }
+}
