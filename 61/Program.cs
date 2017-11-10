f3338e9f3784937fb3b4f1f43527baebcac0ac05
tongjr1 <512519134@qq.com>
2017-11-10 09:31:49 +0800

    ADD 61

---
+++ 61/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            ListNode head;
+            int k = 2;
+            var result = solution.RotateRight(head,k);
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 61/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _61
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            ListNode newBottom = head;
+            List<int> b = new List<int>();
+            int i = 0;
+            ListNode bottom = head;
+            while (i<k)
+            {
+                bottom = bottom.next;
+                i++;
+            }
+            while(bottom != null||bottom.next != null)
+            {
+                bottom = bottom.next;
+                newBottom = newBottom.next;
+            }
+            var newHead = newBottom.next;
+            newBottom.next = null;
+            bottom.next = head;
+            return newHead;
+        }
+    }
+}
